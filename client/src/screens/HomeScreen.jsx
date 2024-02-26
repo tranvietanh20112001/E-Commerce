f5deed7931d06940466f20cdd330b9704d38c40e
Trần Việt Anh <95759256+tranvietanh20112001@users.noreply.github.com>
2024-02-27 04:04:11 +0700

    create server, create database, doing model

--- client/src/screens/HomeScreen.jsx
+++ client/src/screens/HomeScreen.jsx
@@ -1,19 +1,19 @@
-// import { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
-import { products } from "../data/products";
+// import { products } from "../data/products";
 import Product from "../components/Product";
-// import axios from "axios";
+import axios from "axios";
 
 export default function HomeScreen() {
-  // const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState([]);
 
-  // useEffect(() => {
-  //   const fetchProducts = async () => {
-  //     const res = await axios.get("/api/products");
-  //     setProducts(res);
-  //   };
-  //   fetchProducts();
-  // }, []);
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get("/api/products");
+      setProducts(data);
+    };
+    fetchProducts();
+  }, []);
 
   return (
     <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 gap-4">
